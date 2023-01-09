d2ce71373a2792277976f70db2583dbcbc657bc4
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-09 11:33:08 +0530

    Done Task3 description added1

---
+++ task3.py
@@ -0,0 +1,9 @@
+'''
+3. You are given a list of sentences. Create a word tree from each sentence and find how many times the word is appeared in other word trees.
+ Input:
+sentences = [“My name is Ram”, “”He is a good person”, “You should be careful while coding”, “He can do better”, “The person is mysterious”, “Jay Shree Ram”, “It is my pen.”]
+ Output:
+word_trees = [[“My”, “name”, “is”, “Ram”], [“He”, “is”, “a”, “good”, “person”], [“You”, “should”, “be”, “careful”, “while”, “coding”], [“He”, “can”, “do”, “better”], [“The”, “person”, “is”, “mysterious”], [“Jay”, “Shree”, “Ram”], [“It”, “is”, “my”, “pen”]]
+Number of time each word appears:
+{“My”: 1, “name”: 1, “is”: 4, “Ram”: 2, “He”: 2, “good”: 1, “person”: 2, “You”: 1 ......} Likewise all word should be covered.
+'''
