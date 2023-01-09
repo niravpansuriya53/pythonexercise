e054d294607eb47f6f3ebdf5c28cc0be81122e59
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-09 18:10:30 +0530

    Done most_freq and least_freq

--- task2.py
+++ task2.py
@@ -1,55 +1,59 @@
-'''2. You are given a list of person names. Your task is to find out the three most frequent and three least frequent names.
- Input:
-names = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White']
- Output:
-o Names: 'Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller',
-'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White'] o Name lengths: [5, 7, 8, 5, 5, 5, 6, 6, 5, 6, 8, 6, clr7, 5]
-o The three most frequent name lenghts are:
-6 names of length 5: ['Smith', 'Jones', 'Brown', 'Davis', 'Moore'] 
-4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']
-2 names of length 8: ['Williams', 'Anderson']
-o The three least frequent name lenghts are:
-2 names of length 7: ['Johnson', 'Jackson']
-2 names of length 8: ['Williams', 'Anderson']
-4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']'''
+# Person names
+names = [
+    "Smith",
+    "Johnson",
+    "Williams",
+    "Jones",
+    "Brown",
+    "Davis",
+    "Miller",
+    "Wilson",
+    "Moore",
+    "Taylor",
+    "Anderson",
+    "Thomas",
+    "Jackson",
+    "White",
+]
+#Create dictionary name: name length
+names_length = {name: len(name) for name in names}
+print("Length of names :", list(names_length.values()))
 
-names = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White']
-names1=[]
-for i in names:
-    len1= len(i)
-    names1.append(len1)
-print(names1)
-from collections import Counter
-counts = dict(Counter(names1))
-# print(counts)
+# Sorting 
+sorted_names_length = sorted(names_length.items(), key=lambda x: x[1])
+print(sorted_names_length)
 
-myKeys = list(counts.keys())
-myKeys.sort()
-sorted_dict = {i: counts[i] for i in myKeys}
-print(sorted_dict)
+#Create dictionary name length vise value
+length_wise_names = dict()
+for name_length_tuple in sorted_names_length:
+    if name_length_tuple[1] in length_wise_names.keys():
+        length_wise_names[name_length_tuple[1]].append(name_length_tuple[0])
+    else:
+        length_wise_names[name_length_tuple[1]] = [name_length_tuple[0]]
 
-#Finde top three most  frequent name lenght
-top_three = sorted(counts, key=counts.get, reverse=True)[:3]
-# print(top_three)
+# 3 most frequent names
+print(" \nThe three most frequent name lenghts are:")
+three_most_freq_names = dict(sorted(length_wise_names.items())[:3])
+for key,value in three_most_freq_names.items():
+    print(len(value)," names of length ",key ,":",value)
 
-top_three_most=[]
-for top in top_three:
-    for name in names:
-        if len(name) ==  top:
-            top_three_most.append(name)
-print(top_three_most)
-print("length of 5",top_three_most[:6])
-print("length of 6",top_three_most[6:10])
-print("length of 7",top_three_most[10:])
+# 3 least frequent names
+print(" \nThe three least frequent name lenghts are:")
+three_least_freq_names = dict(sorted(length_wise_names.items())[-3:])
+for key,value in three_least_freq_names.items():
+    print(len(value)," names of length ",key ,":",value)
 
-last_three = sorted(counts, key=counts.get, reverse=True)[-3:]
-last_three_most=[]
-print(last_three)
-for last in last_three:
-    for name in names:
-        if len(name) == last:
-            last_three_most.append(name)
-print(last_three_most)
-print("length of 6",last_three_most[:4])
-print("length of 7",last_three_most[4:6])
-print("length of 6",last_three_most[6:])
+#output
+'''
+Length of names : [5, 7, 8, 5, 5, 5, 6, 6, 5, 6, 8, 6, 7, 5]
+ 
+The three most frequent name lenghts are:
+6  names of length  5 : ['Smith', 'Jones', 'Brown', 'Davis', 'Moore', 'White']
+4  names of length  6 : ['Miller', 'Wilson', 'Taylor', 'Thomas']
+2  names of length  7 : ['Johnson', 'Jackson']
+ 
+The three least frequent name lenghts are:
+4  names of length  6 : ['Miller', 'Wilson', 'Taylor', 'Thomas']
+2  names of length  7 : ['Johnson', 'Jackson']
+2  names of length  8 : ['Williams', 'Anderson']
+'''
