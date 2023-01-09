b7dec150c0c41fda83a7a28b3417f05b5fde3918
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-09 14:32:28 +0530

    Done List of List

--- task3.py
+++ task3.py
@@ -7,3 +7,11 @@ word_trees = [[“My”, “name”, “is”, “Ram”], [“He”, “is”, 
 Number of time each word appears:
 {“My”: 1, “name”: 1, “is”: 4, “Ram”: 2, “He”: 2, “good”: 1, “person”: 2, “You”: 1 ......} Likewise all word should be covered.
 '''
+
+sentences = ["My name is Ram","He is a good person","You should be careful while coding","He can do better","The person is mysterious","Jay Shree Ram","It is my pen"]
+# print(sentences)
+list_of_list= []
+for element in sentences:
+    sub = element.split(', ')
+    list_of_list.append(sub)    
+print(list_of_list)
