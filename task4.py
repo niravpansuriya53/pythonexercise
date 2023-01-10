6e984594abb0c3325211c3c614b13c1787db5d79
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-10 09:24:38 +0530

    Task4 discription added

---
+++ task4.py
@@ -0,0 +1,10 @@
+'''
+4. Recursion:
+ Create a recursive function which returns the n-th Fibonacci number. [Fibonacci
+sequence: 1, 1, 2, 3, 5, 8, 13, 21, ...]
+o Input: N=8
+o Output: 21
+o Also, check if your recursive function is able to return the Fibonacci value at 60th or 90th term? If no, then check the concept of memoization for Fibonacci in recursive way.
+ Create a recursion function that calculate the sum of numbers present in the list. o Input:
+numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
+o Output: 152'''
