92b177070b02bc301a7fbb46d52e29ba445cb23a
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-11 18:08:50 +0530

    how many substring occurs is done

--- task6.py
+++ task6.py
@@ -5,3 +5,18 @@ string = “PQRQRQRQ”
 substring = “QRQ”
  Output: 3
 '''
+def count_substring(string, substring):
+    count = 0
+    start = 0
+    while start < len(string):
+        positon = string.find(substring, start)
+        if positon != -1:
+            start = positon + 1
+            count += 1
+        else:
+            break
+    return count
+# Driver Code
+string = "PQRQRQRQ"
+substring = "QRQ"
+print(count_substring(string, substring))
