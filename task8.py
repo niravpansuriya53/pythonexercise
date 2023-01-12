3a318b1287bc670b9c990cc07036517da8b85117
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-12 09:57:09 +0530

    Task8 description added

---
+++ task8.py
@@ -0,0 +1,8 @@
+'''
+8. You are given a list of numbers and your task is to sort the list without any inbuilt method of python.
+ Input:
+numbers = [2, 5, 6, 1, 3, 8, 9, 10]
+ Output:
+[1, 2, 3, 5, 6, 8, 9, 10]
+'''
+
