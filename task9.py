5b11a8baf02678a5d5c81ea16f3e21753c19d878
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-17 09:35:42 +0530

    Task9 description added

---
+++ task9.py
@@ -0,0 +1,3 @@
+'''
+9. Take two datetimes from user start_time and end_time. Calculates the difference between start_time and end_time by removing night intervals (12 AM to 6 AM). (Hint: You can use python intervals module)
+'''
