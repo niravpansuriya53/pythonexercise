2f2618c0e0e70b74907c9111c7e2f2ccf19d5918
Nirav Pansuriya <mac@Taglines-iMac-Pro.local>
2023-01-20 09:31:42 +0530

    Done dynamic arithmetic geometric progress

--- task11.py
+++ task11.py
@@ -8,39 +8,31 @@ One term wrong A.P. = [2, 5, 8, 11, 15, 17] One term wrong G.P. = [3, 9, 27, 81,
  Output:
 Correct A.P. = [2, 5, 8, 11, 14, 17] Correct G.P. = [3, 9, 27, 81, 243, 729]
  '''
+from collections import Counter
 # Arithmetic
-arithmetic_progress_number = int(input("Enter a arithmetic number :- "))
-arithmetic_progress= [2, 5, 8, 11, 15, 17]
-for i in range(len(arithmetic_progress)-1):
-    if arithmetic_progress[i+1] - arithmetic_progress_number != arithmetic_progress[i]:
-        arithmetic_progress[i+1] = arithmetic_progress[i] + arithmetic_progress_number
-print(arithmetic_progress)
+arithmetic_progression = [2, 5, 8, 11, 15, 17, 21]
+dif_between_a_p = [arithmetic_progression[i+1] - arithmetic_progression[i] for i in range(len(arithmetic_progression)-1)]
+#fetch most commne difference number
+count_num = Counter(dif_between_a_p)
+most_feq_number = dict(count_num.most_common(1))
+a_p_num = list(most_feq_number)
+
+for i in range(len(arithmetic_progression)-1):
+    if arithmetic_progression[i+1] - a_p_num[0] != arithmetic_progression[i]:
+        arithmetic_progression[i+1] = arithmetic_progression[i] + a_p_num[0]
+print(arithmetic_progression)
+
 
 #Geometric
-geometric_progress_number = int(input("Enter a geometric number :- "))
-geometric_progress= [3, 9, 27, 81, 244, 729]
-for i in range(len(geometric_progress)-1):
-    if geometric_progress[i+1] / geometric_progress_number != geometric_progress[0]:
-        geometric_progress[i+1] = geometric_progress_number * geometric_progress[i]
-print(geometric_progress)
+geometric_progression= [3, 9, 27, 81, 244, 729]
+div_between_g_p = [geometric_progression[i+1] / geometric_progression[i] for i in range(len(geometric_progression)-1)]
+#fetch most comman divided
+count_num = Counter(div_between_g_p)
+most_feq_number = dict(count_num.most_common(1))
+g_p_num = list(most_feq_number) 
 
-'''
-Logic from this consecutive terms difference (2-1 = 3-2 = 4-3 = 5-4 = 6-5 = 7-6 = 8-7 = 1)  [1, 2, 3, 4, 5, 6, 7, 8]
-'''
-# daynamic list of arithmetic
-arithmetic_progress_number = int(input("Enter a arithmetic number :- "))
-arithmetic_progress= [2, 5, 8, 11, 15, 17]
-arithmetic_progress[0]=arithmetic_progress_number
-for i in range(len(arithmetic_progress)-1):
-    if arithmetic_progress[i+1] - arithmetic_progress_number != arithmetic_progress[i]:
-        arithmetic_progress[i+1] = arithmetic_progress[i] + arithmetic_progress_number
-print(arithmetic_progress)
+for i in range(len(geometric_progression)-1):
+    if geometric_progression[i+1] / g_p_num[0] != geometric_progression[0]:
+        geometric_progression[i+1] = int(g_p_num[0]) * int(geometric_progression[i])
+print(geometric_progression)
 
-# daynamic list of Geometric
-geometric_progress_number = int(input("Enter a geometric number :- "))
-geometric_progress= [3, 9, 27, 81, 244, 729]
-geometric_progress[0] = geometric_progress_number
-for i in range(len(geometric_progress)-1):
-    if geometric_progress[i+1] / geometric_progress_number != geometric_progress[0]:
-        geometric_progress[i+1] = geometric_progress_number * geometric_progress[i]
-print(geometric_progress)
